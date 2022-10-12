ac6589ae7d56df3635e8ee5def11cf90cd291f12
mooujarrar <mohyiddine1997@gmail.com>
2022-10-12 12:43:40 +0200

    Python script reading frames from serialport and plotting them

---
+++ plotter.py
@@ -0,0 +1,44 @@
+from sys import byteorder
+import serial
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+serialPort = serial.Serial(port = "COM3",baudrate=500000, timeout=None) #bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
+serialString = ""                           # Used to hold data coming over UART
+
+xdim = 320
+ydim = 240
+
+bytesPerFrame = 320 * 240 * 2
+
+def drawImage(buff):
+    arr = np.frombuffer(buff,dtype=np.uint16).astype(np.uint32)
+    arr = 0xFF000000 + ((arr & 0xF800) >> 8) + ((arr & 0x07E0) << 5) + ((arr & 0x001F) << 19)
+
+    return Image.frombuffer('RGBA', (xdim, ydim), arr, 'raw','RGBA', 0, 1)
+
+plt.ion()
+
+while(1):
+
+    # Wait until there is data waiting in the serial buffer
+    if(serialPort.in_waiting > 1):
+        # Read data out of the buffer until a carraige return / new line is found
+        serialString = serialPort.read(size=bytesPerFrame)
+        #print(serialString)
+        serialString = int.from_bytes(serialString, byteorder='little').to_bytes(bytesPerFrame, byteorder='big')
+        #print(serialString)
+
+        # Print the contents of the serial data
+        #print(list(serialString))
+        im = drawImage(serialString)
+        # plt.imshow(im)
+        # plt.show()
+        # plt.pause(0.0001)
+        # plt.clf()
+        im.show()
+        #serialPort.reset_input_buffer()
+
+        
